8f2c3baa98314f24033220fa3cdfbabacaf9de2c
Anuj Uike <uikeanuj218@gmail.com>
2026-08-26 22:10:43 +0530

    3150-SHORTEST-AND-LEXICOGRAPHICALLY-SMALLEST-BEAUTIFUL-STRING Stats: Time: 0 ms (100%), Space: 8.7 MB (82.87%) - LeetHub

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        vector<int> onepos;
+        // vector<int> onepos;
+        // string ans = "";
+
+        // for(int i = 0; i < s.size(); i++){
+        //     if(s[i] == '1') onepos.push_back(i);
+        // }
+        // for(int i = 0; i+k-1 < onepos.size(); i++){
+            
+        //     string curr = s.substr(l,r-l+1);
+
+        //     if(ans.empty() || curr.size() < ans.size() || (curr.size() == ans.size() && curr < ans)){
+        //         ans = curr;
+        //     }
+        // }
+        // return ans;
+        int l = 0;
+        int one = 0;
         string ans = "";
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '1') onepos.push_back(i);
-        }
-        for(int i = 0; i+k-1 < onepos.size(); i++){
-            int l = onepos[i];
-            int r = onepos[i+k-1];
+        for(int r = 0; r < s.size(); r++){
+            if(s[r] == '1') one++;
+            while(one > k){
+                if(s[l] == '1') one--;
+                l++;
+            }
+            if(one == k){
+                while(s[l] == '0') l++;
 
-            string curr = s.substr(l,r-l+1);
+                string curr = s.substr(l,r-l+1);
 
-            if(ans.empty() || curr.size() < ans.size() || (curr.size() == ans.size() && curr < ans)){
-                ans = curr;
+                if(ans.empty() || curr.size() < ans.size() || (curr.size() == ans.size() && curr < ans)){
+                             ans = curr;
+                }
+
             }
         }
         return ans;
